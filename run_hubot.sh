1aa13d61a072708614061903a5d48c88e82a8fbc
sdt-nakamura <k_nakamura@sysdt.com>
2016-07-13 17:15:46 +0900

    HuBot実行スクリプト

---
+++ run_hubot.sh
@@ -0,0 +1,5 @@
+export HUBOT_SLACK_TOKEN=xoxb-59252553463-9Exxonh8VZCucv7rdmlxMfru
+export HUBOT_SLACK_TEAM=SDT_BOT
+export HUBOT_SLACK_BOTNAME=my_bot2
+export PORT=9999
+./bin/hubot --adapter slack
